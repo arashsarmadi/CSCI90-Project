bec6a1a160a90e5b3ec9147e5f1209cd3e408ccb
Arash Sarmadi <arash.sarmadi@gmail.com>
2022-12-03 17:24:07 -0500

    added calling function

--- test_code5.py
+++ test_code5.py
@@ -2,3 +2,6 @@ def read_file_noncompliant(filename):
     file = open(filename, 'r')
     # Noncompliant: method returns without properly closing the file.
     return file.readlines()
+
+
+read_file_noncompliant('C:\Data-Science\CS90\Project\CSCI90-Project\.gitignore')
